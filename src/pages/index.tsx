6b8d7a54d20e936a7186b106a239361c87505d35
Vitor Hugo <vitor4oliveira@gmail.com>
2022-02-08 16:23:37 -0300

    Added Layout and Title components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Layout from '../components/Layout'
 
 export default function Home() {
   return (
-    <div className="flex h-screen justify-center items-center">
-      <span className='text-4xl'>Texto</span>
+    <div className={`
+    flex justify-center items-center h-screen bg-gradient-to-r 
+    from-blue-500   to-purple-500 text-white`}>
+
+    <Layout title='Cadastro Simples'><span>Content</span></Layout>
+      
     </div>
   )
 }
