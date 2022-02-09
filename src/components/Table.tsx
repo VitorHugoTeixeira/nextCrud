fdf952ba9bb1cc25373beb4d982ed041ff22daa4
Vitor Hugo <vitor4oliveira@gmail.com>
2022-02-08 23:49:09 -0300

    Layout of table component is complete

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,18 +1,25 @@
 import Client from "../core/Client";
+import { EditIcon, TrashIcon } from "./Icons";
 
 interface TableProps {
     listClients: Client[]
+    selectedClient?: (client: Client) => void
+    excludedClient?: (client: Client) => void
 }
 
 
 export default function Table(props: TableProps) {
 
+    const displayActions = props.excludedClient || props.selectedClient
+
+
     function renderHeader() {
         return (
             <tr>
-                <td>Código</td>
-                <td>Nome</td>
-                <td>Idade</td>
+                <th className={`text-left p-4`}>Código</th>
+                <th className={`text-left p-4`}>Nome</th>
+                <th className={`text-left p-4`}>Idade</th>
+                {displayActions ? <th className={`p-4`}>Ações</th> : false}
             </tr>
         )
     }
@@ -20,20 +27,60 @@ export default function Table(props: TableProps) {
     function renderData() {
         return props.listClients?.map((client, i) => {
             return (
-                <tr key={client.id}>
-                    <td>{client.id}</td>
-                    <td>{client.name}</td>
-                    <td>{client.age}</td>
+                <tr key={client.id} className={`
+                ${i % 2 === 0 ? 'bg-purple-200' : 'bg-purple-100'}
+                `}>
+                    <td className={`text-left p-4`}>{client.id}</td>
+                    <td className={`text-left p-4`}>{client.name}</td>
+                    <td className={`text-left p-4`}>{client.age}</td>
+                    {displayActions ? renderActions(client) : false}
                 </tr>
             )
         })
     }
 
+    function renderActions(client: Client) {
+        return (
+            <td className="flex justify-center">
+
+                {props.selectedClient ? (
+                    <button onClick={() => props.selectedClient?.(client)} className={`
+                        flex justify-center items-center
+                      text-green-600 rounded-full p-2 m-1
+                      hover:bg-purple-50 
+                    `}>
+                        {EditIcon}
+                    </button>
+
+                ) : false}
+
+                {props.excludedClient ? (
+                    <button onClick={() => props.excludedClient?.(client)} className={`
+                        flex justify-center items-center
+                      text-red-600 rounded-full p-2 m-1
+                      hover:bg-purple-50 
+                    `}>
+                        {TrashIcon}
+                    </button>
+                ) : false}
+
+
+            </td>
+        )
+    }
+
 
     return (
-        <table>
-            <thead>{renderHeader()}</thead>
-            <tbody>{renderData()}</tbody>
+        <table className={`w-full rounded overflow-hidden`}>
+            <thead className={`
+                text-gray-100
+                bg-gradient-to-r from-purple-500 to-purple-800
+            `}>
+                {renderHeader()}
+            </thead>
+            <tbody>
+                {renderData()}
+            </tbody>
         </table>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,22 +5,29 @@ import Table from '../components/Table'
 import Client from '../core/Client'
 
 export default function Home() {
-  
+
   const listClients = [
     new Client('Vitor', 22, '1'),
     new Client('Vitoria', 25, '2'),
     new Client('Vitoriano', 23, '3'),
-    new Client('Vitoriana', 24, '4'),
+    new Client('Vitoriana', 24, '4')
   ]
-  
-  
+
+  function selectedClient(client: Client) {
+    console.log(client.name)
+  }
+
+  function excludedClient(client: Client) {
+    console.log(client.name)
+  }
   return (
     <div className={`
     flex justify-center items-center h-screen bg-gradient-to-r 
     from-blue-500   to-purple-500 text-white`}>
 
-    <Layout title='Cadastro Simples'><Table listClients={listClients}></Table></Layout>
-      
+      <Layout title='Cadastro Simples'><Table listClients={listClients}
+      selectedClient={selectedClient} excludedClient={excludedClient}></Table></Layout>
+
     </div>
   )
 }
